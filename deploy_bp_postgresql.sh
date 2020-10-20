1c4af2f1be3ef00f6d5a12268cf4b1752125db86
Josh Long <josh@joshlong.com>
2020-10-20 13:48:17 -0700

    deploy

--- deploy_bp_postgresql.sh
+++ deploy_bp_postgresql.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 APP_NAME=bp-postgresql
 CONTAINER_NAME=postgres
 
@@ -13,3 +15,23 @@ CONTAINER_NAME=postgres
 #kubectl set env deployment/${APP_NAME}  POSTGRES_USER=bp
 #kubectl expose deployment ${APP_NAME} --port=5432 --name=postgres-exposed -o yaml
 #kubectl get deployment -l app=$APP_NAME -o yaml > ${APP_NAME}.yaml
+
+## todo move these to separate config
+export POSTGRES_USER="bp"
+export POSTGRES_PASSWORD="sd@s32s1234s2329sdgfiuesn"
+echo "the PostgreSQL password is ${POSTGRES_PASSWORD} and the PostgreSQL user is ${POSTGRES_USER}"
+
+kubectl apply -f <(echo "
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ${APP_NAME}-secrets
+type: Opaque
+stringData:
+  POSTGRES_USER: ${POSTGRES_USER}
+  POSTGRES_PASSWORD: ${POSTGRES_PASSWORD}
+")
+
+kubectl apply -f bp-postgresql.yaml
+
