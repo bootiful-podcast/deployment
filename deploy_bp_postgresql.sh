143148e6c2dda65dfcc8b46cc795a4cfc3527493
Josh Long <josh@joshlong.com>
2020-10-28 04:18:16 -0700

    deployment renamed

--- deploy_bp_postgresql.sh
+++ deploy_bp_postgresql.sh
@@ -3,19 +3,6 @@
 APP_NAME=bp-postgresql
 CONTAINER_NAME=postgres
 
-# docker run -p 5432:5432 --name $DB_NAME -e POSTGRES_USER=bp -e POSTGRES_PASSWORD=bp -d postgres
-
-# kubectl run $APP_NAME \
-#   --image=${CONTAINER_NAME} \
-#   --replicas=1 \
-#   --port=5432 
-
-#kubectl create deployment --image=${CONTAINER_NAME} ${APP_NAME}
-#kubectl set env deployment/${APP_NAME}  POSTGRES_PASSWORD=bp
-#kubectl set env deployment/${APP_NAME}  POSTGRES_USER=bp
-#kubectl expose deployment ${APP_NAME} --port=5432 --name=postgres-exposed -o yaml
-#kubectl get deployment -l app=$APP_NAME -o yaml > ${APP_NAME}.yaml
-
 kubectl apply -f <(echo "
 ---
 apiVersion: v1
--- deploy_bp_rabbitmq.sh
+++ deploy_bp_rabbitmq.sh
@@ -2,13 +2,6 @@
 
 APP_NAME=bp-rabbitmq
 
-# kill the existing container
-#cid=$( docker ps -a | grep $APP_NAME | cut -f1 -d\ )
-#docker rm -f $cid
-
-# https://codeburst.io/get-started-with-rabbitmq-on-docker-4428d7f6e46b
-#docker run --rm -it --hostname ${APP_NAME}-leader-node -p 15672:15672 -p 5672:5672 rabbitmq:3.8.9-management
-
 for i in deployment service configmap secret ; do
   kubectl delete $i $(kubectl get $i | grep $APP_NAME | cut -f1 -d\ ) || \
       echo "could not find an existing $i to delete for app $APP_NAME"
