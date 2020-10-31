e51cd1eceb85d5c477867e2f8766a5161eb8879c
Josh Long <josh@joshlong.com>
2020-10-31 05:43:23 -0700

    gke

--- deploy_bp_gke_cluster.sh
+++ deploy_bp_gke_cluster.sh
@@ -8,11 +8,11 @@ GKE_CLUSTER_NAME=bootiful-podcast-${SUFFIX}
 function deploy_new_gke_cluster(){
 
   ## first lets initialize the cluster
-  gcloud  --quiet  beta container --project $GKE_PROJECT clusters create "${GKE_CLUSTER_NAME}" \
+  gcloud --quiet  beta container --project $GKE_PROJECT clusters create "${GKE_CLUSTER_NAME}" \
      --zone "us-central1-c" --no-enable-basic-auth --cluster-version "1.16.13-gke.401" \
-     --machine-type "e2-standard-2" --image-type "COS" --disk-type "pd-standard" --disk-size "100" \
+     --machine-type "c2-standard-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" \
      --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append","https://www.googleapis.com/auth/ndev.clouddns.readwrite" \
-     --num-nodes "35" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/${GKE_PROJECT}/global/networks/default" \
+     --num-nodes "3" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/${GKE_PROJECT}/global/networks/default" \
      --subnetwork "projects/${GKE_PROJECT}/regions/us-central1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks \
      --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
 
