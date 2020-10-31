dabdeb735798dbff77b0c2c6e8c44ade8a668363
Josh Long <josh@joshlong.com>
2020-10-31 04:14:53 -0700

    larger cluster please Sat Oct 31 04:14:53 PDT 2020

--- deploy_bp_gke_cluster.sh
+++ deploy_bp_gke_cluster.sh
@@ -12,7 +12,7 @@ function deploy_new_gke_cluster(){
      --zone "us-central1-c" --no-enable-basic-auth --cluster-version "1.16.13-gke.401" \
      --machine-type "e2-standard-2" --image-type "COS" --disk-type "pd-standard" --disk-size "100" \
      --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append","https://www.googleapis.com/auth/ndev.clouddns.readwrite" \
-     --num-nodes "5" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/${GKE_PROJECT}/global/networks/default" \
+     --num-nodes "35" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/${GKE_PROJECT}/global/networks/default" \
      --subnetwork "projects/${GKE_PROJECT}/regions/us-central1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks \
      --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
 
