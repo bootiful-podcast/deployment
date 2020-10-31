179c33f0a8dce09a3a508374c6a30e5b1264c286
Josh Long <josh@joshlong.com>
2020-10-31 04:05:03 -0700

    larger cluster please Sat Oct 31 04:05:03 PDT 2020

--- deploy_bp_gke_cluster.sh
+++ deploy_bp_gke_cluster.sh
@@ -8,7 +8,7 @@ GKE_CLUSTER_NAME=bootiful-podcast-${SUFFIX}
 function deploy_new_gke_cluster(){
 
   ## first lets initialize the cluster
-  gcloud beta container --project $GKE_PROJECT clusters create "${GKE_CLUSTER_NAME}" \
+  gcloud  --quiet  beta container --project $GKE_PROJECT clusters create "${GKE_CLUSTER_NAME}" \
      --zone "us-central1-c" --no-enable-basic-auth --cluster-version "1.16.13-gke.401" \
      --machine-type "e2-standard-2" --image-type "COS" --disk-type "pd-standard" --disk-size "100" \
      --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append","https://www.googleapis.com/auth/ndev.clouddns.readwrite" \
