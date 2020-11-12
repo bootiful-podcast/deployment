b91762078bbb259f965cbb4c7af8fa6e23924f6c
Josh Long <josh@joshlong.com>
2020-11-11 21:18:06 -0800

    repository

--- .github/workflows/repository_utils.sh
+++ .github/workflows/repository_utils.sh
@@ -4,10 +4,11 @@ echo "Loaded repository utils"
 
 function deploy_system_app(){
   APP_NAME=$1
+  echo "Trying to invoke the deployment for ${APP_NAME}."
   RESULT=$( curl \
     -X POST \
     -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token $GH_PERSONAL_ACCESS_TOKEN" \
+    -H "Authorization: token ${GH_PERSONAL_ACCESS_TOKEN}" \
     https://api.github.com/repos/bootiful-podcast/${APP_NAME}/dispatches \
     -d '{"event_type":"deploy-event"}'
   )
