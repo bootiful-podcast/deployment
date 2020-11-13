73242718b7ff3dfa2bebba7409c58ab1ffefb822
Josh Long <josh@joshlong.com>
2020-11-12 17:23:05 -0800

    update dns

--- dns/deploy.sh
+++ dns/deploy.sh
@@ -4,10 +4,14 @@ set -e
 set -o pipefail
 
 echo "Applying DNS configuration in $ROOT_DIR "
-
-RESERVED_IP_NAME=bootiful-podcast-ip
+RESERVED_IP_NAME=bootiful-podcast-${BP_MODE_LOWERCASE}-ip
 gcloud compute addresses list --format json | jq '.[].name' -r | grep $RESERVED_IP_NAME || \
   gcloud compute addresses create $RESERVED_IP_NAME --global
 
-kubectl apply -f ${ROOT_DIR}/dns
 
+KC=${ROOT_DIR}/dns/overlays/${BP_MODE_LOWERCASE}/
+
+# ls -la $KC
+# echo $KC
+
+kubectl apply -k $KC
