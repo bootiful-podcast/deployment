82b0cdf2593217ac30cd0c8c29b7160d58106447
Josh Long <josh@joshlong.com>
2020-11-12 22:25:01 -0800

    dns

--- .github/workflows/config_client.sh
+++ .github/workflows/config_client.sh
@@ -46,7 +46,9 @@ function hydrate_environment_from_config_server() {
   config-client $CONFIGURATION_SERVER_USERNAME $CONFIGURATION_SERVER_PASSWORD deployment \
     $BP_MODE_LOWERCASE http://${IP_OF_CONFIG_SERVER} $GITHUB_ENV
 
-  cat $GITHUB_ENV | while read l; do eval "export $l ;" done ;
+  cat $GITHUB_ENV | while read l ; do
+    eval "export $l ;" ;
+  done
 
   echo "------"
 }
