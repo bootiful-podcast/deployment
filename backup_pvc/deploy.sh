a32ac635970073c64f5783b27a9df1149fbd6d5c
Josh Long <josh@joshlong.com>
2020-11-12 18:14:25 -0800

    pvcs

--- backup_pvc/deploy.sh
+++ backup_pvc/deploy.sh
@@ -5,9 +5,10 @@ set -o pipefail
 
 echo "Deploying Backup PVC..."
 
-FS_NAME="bp-backup-disk"
+FS_NAME="bp-${BP_MODE_LOWERCASE}-backup-disk"
 
 gcloud --quiet beta compute disks describe $FS_NAME --zone $GCLOUD_ZONE ||
   gcloud --quiet beta compute disks create $FS_NAME --type=pd-balanced --size=200GB --zone $GCLOUD_ZONE
 
-kubectl apply -f $ROOT_DIR/backup_pvc/backup_pvc.yaml
+KC=${ROOT_DIR}/backup_pvc/overlays/${BP_MODE_LOWERCASE}/
+kubectl apply -k $KC
