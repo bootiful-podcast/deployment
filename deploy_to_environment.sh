f2347e3af0c266043b1cc84a138958dd65c61e2b
Josh Long <josh@joshlong.com>
2020-11-13 21:07:53 -0800

    deploy

--- deploy_to_environment.sh
+++ deploy_to_environment.sh
@@ -4,4 +4,5 @@ set -o pipefail
 
 source .github/workflows/repository_utils.sh
 export BP_MODE_LOWERCASE=${1:-development}
+export GH_PERSONAL_ACCESS_TOKEN=${GH_PERSONAL_ACCESS_TOKEN:-$GITHUB_PERSONAL_ACCESS_TOKEN}
 deploy_system_app deployment
