3715fb28061a524764c9deac9c6ed27f08a44b0e
Josh Long <josh@joshlong.com>
2020-11-13 20:54:37 -0800

    deploy

--- deploy_to_environment.sh
+++ deploy_to_environment.sh
@@ -3,5 +3,5 @@ set -e
 set -o pipefail
 
 source .github/workflows/repository_utils.sh
-export BP_MODE_LOWERCASE=${BP_MODE_LOWERCASE:-development}
+export BP_MODE_LOWERCASE=${1:-development}
 deploy_system_app deployment
