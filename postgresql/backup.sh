bcdb0df1ea1f9f517ea4bfe8b846adef30aad40f
Josh Long <josh@joshlong.com>
2020-11-14 18:24:51 -0800

    config

--- postgresql/backup.sh
+++ postgresql/backup.sh
@@ -4,6 +4,6 @@ set -e
 set -o pipefail
 
 echo "Manually starting an instance of the backup-cronjob"
-JOB_ID=backup-cronjob-initial-run-job
+JOB_ID=backup-cronjob-initial-run-job-${RANDOM}
 kubectl get jobs | grep $JOB_ID && kubectl delete  jobs/$JOB_ID
 kubectl create job --from=cronjob/backup-cronjob $JOB_ID
--- postgresql/deploy.sh
+++ postgresql/deploy.sh
@@ -13,13 +13,12 @@ SECRETS_NAME=${APP_NAME}-secrets
 
 kubectl delete deployment.apps/${APP_NAME} || echo "Could not delete the existing deployment"
 
-kubectl delete service/postgres || echo "Could not delete old service"
-kubectl delete deployment.apps/postgresql || echo "Could not delete old deployment "
-kubectl delete secrets/${SECRETS_NAME} || echo "Could not delete old ${SECRETS_NAME}."
-kubectl delete configmap/${CONFIG_MAP_NAME} || echo "Could not delete old ${CONFIG_MAP_NAME}."
-
-kubectl get configmap/${CONFIG_MAP_NAME} | grep $CONFIG_MAP_NAME || kubectl create configmap $CONFIG_MAP_NAME --from-file $ROOT_DIR/postgresql/bin/
-kubectl get configmap/${CONFIG_MAP_NAME} -o yaml
+#kubectl delete service/postgres || echo "Could not delete old service"
+#kubectl delete deployment.apps/postgresql || echo "Could not delete old deployment "
+#kubectl delete secrets/${SECRETS_NAME} || echo "Could not delete old ${SECRETS_NAME}."
+#kubectl delete configmap/${CONFIG_MAP_NAME} || echo "Could not delete old ${CONFIG_MAP_NAME}."
+#kubectl get configmap/${CONFIG_MAP_NAME} | grep $CONFIG_MAP_NAME || kubectl create configmap $CONFIG_MAP_NAME --from-file $ROOT_DIR/postgresql/bin/
+#kubectl get configmap/${CONFIG_MAP_NAME} -o yaml > configmap.yaml
 
 kubectl apply -f <(echo "
 ---
@@ -33,5 +32,5 @@ stringData:
   POSTGRES_USER: ${BP_POSTGRES_USERNAME}
   POSTGRES_PASSWORD: ${BP_POSTGRES_PASSWORD}
 ")
-
+kubectl apply -f configmap.yaml
 kubectl apply -f postgresql.yaml
