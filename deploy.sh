0facc8d7813575b5feda14257b1fea5b556ab322
Josh Long <josh@joshlong.com>
2020-11-25 18:10:10 -0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,11 @@ $ROOT_DIR/postgresql/deploy.sh
 $ROOT_DIR/rabbitmq/deploy.sh
 $ROOT_DIR/backup_cronjob/deploy.sh
 
-deploy_system_app studio-v2
+# deploy_system_app site-generator-v2
 deploy_system_app api-v2
-deploy_system_app site-generator-v2
 deploy_system_app processor-v2
 deploy_system_app site-v1
-
+deploy_system_app studio-v2
 
 ## TODO   We don't know how long it'll take for all the various apps to spin up \
 #         and register healthy on their health checks so that DNS/ingress work out.
