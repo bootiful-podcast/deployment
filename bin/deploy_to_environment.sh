e7024c4384cc0b3f7e2eed6bbf75b2498f39a580
Josh Long <josh@joshlong.com>
2020-12-03 12:30:50 -0800

    deploy

--- bin/deploy_to_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -o pipefail
-
-source .github/workflows/repository_utils.sh
-export BP_MODE_LOWERCASE=${1:-development}
-export GH_PERSONAL_ACCESS_TOKEN=${GH_PERSONAL_ACCESS_TOKEN:-$GITHUB_PERSONAL_ACCESS_TOKEN}
-deploy_system_app deployment
--- bin/rebuild_site_and_search_index.sh
+++ bin/rebuild_site_and_search_index.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-token=$(curl -XPOST -u jlong https://api.bootifulpodcast.online/token ) 
-curl -H"Authorization: bearer $token " -XDELETE https://api.bootifulpodcast.online/admin/caches 
+## basically this will trigger a refresh in prod unless you opt in to the development env 
+THE_ENV=""
+if [ "${1}" = "" ] ; then 
+ THE_ENV=""
+else 
+ THE_ENV="${1}."
+fi 
+
+URI_ROOT=https://api.${THE_ENV}bootifulpodcast.fm
+echo "contacting $URI_ROOT"
+TOKEN=$(curl -XPOST -u jlong ${URI_ROOT}/token ) 
+curl -H"Authorization: bearer $TOKEN " -XDELETE ${URI_ROOT}/admin/caches 
